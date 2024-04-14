5027bbebf8ee776126e0c694236b8824aff90b0e
gsahab0007 <gsahab0007@gmail.com>
2024-04-14 11:29:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ['*'],
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+//     credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/leave', leaveRoute);
