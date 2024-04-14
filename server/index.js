15b82974bff5f5335a2499312051f30a48a7a0de
gsahab0007 <gsahab0007@gmail.com>
2024-04-14 09:04:13 +0530

    url updations

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [""]
+}));
 app.use(express.json());
 
 app.use('/api/v1/leave', leaveRoute);
